eea96e57e6c760f042d877a7e6d4b9593ce26944
lurian <lurian-code@protonmail.com>
2022-12-20 23:26:18 +0100

    stuff

---
+++ linux-scripts/setup-linux.sh
@@ -0,0 +1,27 @@
+sudo apt update && sudo apt upgrade -y
+cd Downloads
+flatkpak install org.telegram.desktop
+wget https://dl.discordapp.net/apps/linux/0.0.21/discord-0.0.21.deb
+sudo apt install ./discord-0.0.21.deb
+ls -al /dev/kvm
+sudo usermod -aG kvm $USER
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+wget https://desktop.docker.com/linux/main/amd64/docker-desktop-4.15.0-amd64.deb
+sudo apt-get install ./docker-desktop-4.15.0-amd64.deb
+flatpak install com.spotify.Client
+flatpak install org.remmina.Remmina
+sudo apt install snapd
+sudo snap install nordpass
+sudo apt install neovim
+sudo apt-get install zsh -y
+sudo chsh -s /usr/bin/zsh
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
--- nvim/.vimrc
+++ nvim/.vimrc
@@ -5,18 +5,21 @@ set scrolloff=8
 set incsearch
 set clipboard=unnamedplus
 set smartindent
-set wrap!
-set expandtab
+set wrap! 
+set expandtab 
 set tabstop=4
 set softtabstop=4
 set undofile
+"Mappings
+nnoremap <C-d> <C-d>zz
+nnoremap <C-u> <C-u>zz
+nnoremap n nzzzv
+nnoremap N Nzzzv
+nnoremap J mzJ'z
 
-"Mappings"
 
-nmap <C-d> <C-d>zz
-nmap <C-u> <C-u>zz
-nmap n nzzzv
-nmap N Nzzzv
+vmap J :m'>+1<CR>gv=gv
+vmap K :m'<-2<CR>gv=gv
 
 
 
