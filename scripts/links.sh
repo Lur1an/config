5cf7895b3786c0d05320631e5e286f17a5e7e4a7
lurian <lurian-code@protonmail.com>
2023-06-24 23:40:28 +0200

    "Update links.sh script to include new directories and remove unused files."

--- scripts/links.sh
+++ scripts/links.sh
@@ -2,7 +2,9 @@ rm -rf ~/.ideavimrc
 rm -rf ~/.config/nvim
 rm -rf ~/.zshrc
 rm -rf ~/.tmux.conf
-rm -rf ~/.config/alacritty/alacritty.yml
+rm -rf ~/.config/alacritty
+rm -rf ~/.themes
+rm -rf ~/.xresources
 (cd ../nvim; ln -s $(pwd) ~/.config/nvim)
 (cd ../intellij; ln -s $(pwd)/.ideavimrc ~/.ideavimrc)
 (cd ../tmux; ln -s $(pwd)/.tmux.conf ~/.tmux.conf)
@@ -10,4 +12,5 @@ mkdir ~/.config/alacritty
 (cd ../alacritty; ln -s $(pwd)/alacritty.yml ~/.config/alacritty/alacritty.yml)
 (cd ../zsh/; ln -s $(pwd)/.zshrc ~/.zshrc)
 (cd ../zsh/; ln -s $(pwd)/.xresources ~/.xresources)
+(cd ../gnome-themes/; ln -s $(pwd)/ ~/.themes)
 
